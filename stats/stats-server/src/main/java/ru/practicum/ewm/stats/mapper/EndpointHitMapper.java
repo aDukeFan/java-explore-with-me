9ae368cc0db00750eb537ce4e66f5b9f1bf19c58
aDukeFan <luckyloki666@ya.ru>
2024-07-30 18:14:45 +0400

    D-1.6 fix mapper style

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/mapper/EndpointHitMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/mapper/EndpointHitMapper.java
@@ -1,13 +1,13 @@
 package ru.practicum.ewm.stats.mapper;
 
-import ru.practicum.ewm.dto.stats.EndpointHitDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import ru.practicum.ewm.dto.stats.EndpointHitDto;
 import ru.practicum.ewm.stats.model.EndpointHit;
 
 @Mapper(componentModel = "spring")
 public interface EndpointHitMapper {
 
-    @Mapping(target="timestamp", source="hitDto.timestamp", dateFormat="yyyy-MM-dd HH:mm:ss")
+    @Mapping(target = "timestamp", source = "hitDto.timestamp", dateFormat = "yyyy-MM-dd HH:mm:ss")
     EndpointHit toSave(EndpointHitDto hitDto);
 }
